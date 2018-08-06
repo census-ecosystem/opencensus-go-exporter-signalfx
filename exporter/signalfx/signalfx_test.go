6858a01d91d5a28dbd653ba1342ba21c94bc4b43
Vitor Casadei <vitor.casadei@gmail.com>
2018-08-06 00:09:56 -0300

    Adding new tests

--- exporter/signalfx/signalfx_test.go
+++ exporter/signalfx/signalfx_test.go
@@ -15,8 +15,124 @@
 package signalfx
 
 import (
+	"context"
 	"testing"
+	"time"
+
+	"go.opencensus.io/stats"
+
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/signalfx/com_signalfx_metrics_protobuf"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
 )
+
+type mSlice []*stats.Int64Measure
+
+func (measures *mSlice) createAndAppend(name, desc, unit string) {
+	m := stats.Int64(name, desc, unit)
+	*measures = append(*measures, m)
+}
+
+type vCreator []*view.View
+
+func (vc *vCreator) createAndAppend(name, description string, keys []tag.Key, measure stats.Measure, agg *view.Aggregation) {
+	v := &view.View{
+		Name:        name,
+		Description: description,
+		TagKeys:     keys,
+		Measure:     measure,
+		Aggregation: agg,
+	}
+	*vc = append(*vc, v)
+}
+
+type TestDatapoint struct {
+	MetricType       string
+	MetricName       string
+	MetricValue      string
+	MetricDimensions []Dimension
+}
+
+type Dimension struct {
+	Key   *string
+	Value *string
+}
+
+var MetricTypeName = map[int32]string{
+	0: "GAUGE",
+	1: "COUNTER",
+	2: "ENUM",
+	3: "CUMULATIVE_COUNTER",
+}
+
+var dataPoints []TestDatapoint
+var token = false
+
+func addDataPoint(dataPoints []TestDatapoint, dataPoint TestDatapoint) []TestDatapoint {
+	exists := false
+	if len(dataPoints) == 0 {
+		dataPoints = append(dataPoints, dataPoint)
+	} else {
+		for _, point := range dataPoints {
+			if point.MetricName == dataPoint.MetricName {
+				exists = true
+			}
+		}
+		if !exists {
+			dataPoints = append(dataPoints, dataPoint)
+		}
+	}
+	return dataPoints
+}
+
+func decodeToken(req *http.Request, tokenValue string) {
+	var header []string
+	for name, headers := range req.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			header = append(header, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+	value := strings.Join(header, "\n")
+	if strings.Contains(value, tokenValue) {
+		token = true
+	}
+}
+
+func decodeDatapoints(seenBodyPoints com_signalfx_metrics_protobuf.DataPointUploadMessage) {
+	for i := 0; i < len(seenBodyPoints.Datapoints); i++ {
+		var dimensions []Dimension
+		if len(seenBodyPoints.Datapoints[i].GetDimensions()) != 0 {
+			for _, dimension := range seenBodyPoints.Datapoints[i].GetDimensions() {
+				dimensions = append(dimensions,
+					Dimension{
+						Key:   dimension.Key,
+						Value: dimension.Value,
+					})
+			}
+		} else {
+			dimensions = nil
+		}
+
+		dataPoint := TestDatapoint{
+			MetricName: *seenBodyPoints.Datapoints[i].Metric,
+			MetricValue: strconv.FormatFloat(*seenBodyPoints.Datapoints[i].Value.DoubleValue,
+				'g', 1, 64),
+			MetricType:       MetricTypeName[int32(*seenBodyPoints.Datapoints[i].MetricType)],
+			MetricDimensions: dimensions,
+		}
+		dataPoints = addDataPoint(dataPoints, dataPoint)
+	}
+}
 
 func TestExporterTokenSet(t *testing.T) {
 	_, err := NewExporter(Options{Token: "token"})
@@ -34,4 +150,216 @@ func TestExporterTokenNotSet(t *testing.T) {
 			t.Fatal("expected empty token error message")
 		}
 	}
+}
+
+func TestGaugeDataOutput(t *testing.T) {
+	dataPoints = nil
+	tokenValue := "opencensusT0k3n"
+	retString := `"OK"`
+	retCode := http.StatusOK
+	var blockResponse chan struct{}
+	var cancelCallback func()
+
+	seenBodyPoints := &com_signalfx_metrics_protobuf.DataPointUploadMessage{}
+	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		bodyBytes := bytes.Buffer{}
+		_, err := io.Copy(&bodyBytes, req.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Body.Close()
+		proto.Unmarshal(bodyBytes.Bytes(), seenBodyPoints)
+
+		decodeDatapoints(*seenBodyPoints)
+		decodeToken(req, tokenValue)
+
+		rw.WriteHeader(retCode)
+		io.WriteString(rw, retString)
+		if blockResponse != nil {
+			if cancelCallback != nil {
+				cancelCallback()
+			}
+			select {
+			case <-req.Context().Done():
+			case <-blockResponse:
+			}
+		}
+	})
+
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+
+	server := http.Server{
+		Handler: handler,
+	}
+	serverDone := make(chan struct{})
+	go func() {
+		if err := server.Serve(l); err == nil {
+			t.Log("I expect serve to eventually error")
+		}
+		close(serverDone)
+	}()
+
+	exporter, err := NewExporter(Options{Token: tokenValue,
+		DatapointEndpoint: "http://" + l.Addr().String()})
+	if err != nil {
+		t.Fatalf("failed to create signalfx exporter: %v", err)
+	}
+
+	view.RegisterExporter(exporter)
+
+	names := []string{"las", "cos", "tar"}
+
+	var measures mSlice
+	for _, name := range names {
+		measures.createAndAppend("metric."+name, name, "")
+	}
+
+	var vc vCreator
+	for _, m := range measures {
+		vc.createAndAppend(m.Name(), m.Description(), nil, m, view.LastValue())
+	}
+
+	if err := view.Register(vc...); err != nil {
+		t.Fatalf("failed to create views: %v", err)
+	}
+	defer view.Unregister(vc...)
+
+	view.SetReportingPeriod(time.Millisecond)
+
+	for _, m := range measures {
+		stats.Record(context.Background(), m.M(1))
+		<-time.After(10 * time.Millisecond)
+	}
+
+	if !token {
+		t.Fatal("Token not properly set")
+	}
+
+	if len(dataPoints) != len(measures) {
+		t.Fatalf("expected %d, but got %d dataPoints", len(measures), len(dataPoints))
+	}
+
+	for _, name := range names {
+		correct := false
+		metricName := "metric_" + name
+		for _, point := range dataPoints {
+			if metricName == point.MetricName &&
+				point.MetricValue == "1" &&
+				point.MetricType == "GAUGE" {
+				correct = true
+			}
+		}
+		if !correct {
+			t.Fatalf("expected metric name to be %s, "+
+				"metric value to be 1 and metricType to be GAUGE", metricName)
+		}
+	}
+
+	l.Close()
+}
+
+func TestCounterDataOutput(t *testing.T) {
+	dataPoints = nil
+	tokenValue := "S3cr3tT0k3n"
+	retString := `"OK"`
+	retCode := http.StatusOK
+	var blockResponse chan struct{}
+	var cancelCallback func()
+
+	seenBodyPoints := &com_signalfx_metrics_protobuf.DataPointUploadMessage{}
+	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		bodyBytes := bytes.Buffer{}
+		_, err := io.Copy(&bodyBytes, req.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Body.Close()
+		proto.Unmarshal(bodyBytes.Bytes(), seenBodyPoints)
+
+		decodeDatapoints(*seenBodyPoints)
+		decodeToken(req, tokenValue)
+
+		rw.WriteHeader(retCode)
+		io.WriteString(rw, retString)
+		if blockResponse != nil {
+			if cancelCallback != nil {
+				cancelCallback()
+			}
+			select {
+			case <-req.Context().Done():
+			case <-blockResponse:
+			}
+		}
+	})
+
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+
+	server := http.Server{
+		Handler: handler,
+	}
+	serverDone := make(chan struct{})
+	go func() {
+		if err := server.Serve(l); err == nil {
+			t.Log("I expect serve to eventually error")
+		}
+		close(serverDone)
+	}()
+
+	exporter, err := NewExporter(Options{Token: tokenValue,
+		DatapointEndpoint: "http://" + l.Addr().String()})
+	if err != nil {
+		t.Fatalf("failed to create signalfx exporter: %v", err)
+	}
+
+	view.RegisterExporter(exporter)
+
+	names := []string{"foo", "bar"}
+
+	var measures mSlice
+	for _, name := range names {
+		measures.createAndAppend("test."+name, name, "")
+	}
+
+	var vc vCreator
+	for _, m := range measures {
+		vc.createAndAppend(m.Name(), m.Description(), nil, m, view.LastValue())
+	}
+
+	if err := view.Register(vc...); err != nil {
+		t.Fatalf("failed to create views: %v", err)
+	}
+	defer view.Unregister(vc...)
+
+	view.SetReportingPeriod(time.Millisecond)
+
+	for _, m := range measures {
+		stats.Record(context.Background(), m.M(1))
+		<-time.After(10 * time.Millisecond)
+	}
+
+	if !token {
+		t.Fatal("Token not properly set")
+	}
+
+	if len(dataPoints) != len(measures) {
+		t.Fatalf("expected %d, but got %d dataPoints", len(measures), len(dataPoints))
+	}
+
+	for _, name := range names {
+		correct := false
+		metricName := "test_" + name
+		for _, point := range dataPoints {
+			if metricName == point.MetricName &&
+				point.MetricValue == "1" &&
+				point.MetricType == "GAUGE" {
+				correct = true
+			}
+		}
+		if !correct {
+			t.Fatalf("expected metric name to be %s, "+
+				"metric value to be 1 and metricType to be GAUGE", metricName)
+		}
+	}
+
+	l.Close()
 }
